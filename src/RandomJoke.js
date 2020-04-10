f2ac66f9ecf67f627a34091f1e229808b14962b5
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2020-04-10 17:32:24 +0300

    fix possible unmount while fetch issue

--- src/RandomJoke.js
+++ src/RandomJoke.js
@@ -1,15 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
 export default function RandomJoke({ more, loadMore }) {
   const [joke, setJoke] = useState("");
+  const componentIsMounted = useRef(true);
+
+  useEffect(() => {
+    // each useEffect can return a cleanup function
+    return () => {
+      componentIsMounted.current = false;
+    };
+  }, []); // no extra deps => the cleanup function run this on component unmount
 
   useEffect(() => {
     async function fetchJoke() {
       try {
         const asyncResponse = await axios("https://api.icndb.com/jokes/random");
         const { value } = asyncResponse.data;
-        setJoke(value.joke);
+
+        if (componentIsMounted.current) {
+          setJoke(value.joke);
+        }
       } catch (err) {
         console.error(err);
       }
